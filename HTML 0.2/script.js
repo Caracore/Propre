f63b98f5ac6a3be9b9258ffc70320da9b1883b81
Caracore <161318324+Caracore@users.noreply.github.com>
2024-10-26 19:30:26 +0200

    Version 0.2
    Test ! Code plus ou moins propre...

---
+++ HTML 0.2/script.js
@@ -0,0 +1,8 @@
+a = "https://vercel.com/caracores-projects/test/CaAgzWmH9faFNZQbad7uxm8FBUAJ"
+
+function ouvrir_un_lien_vercel(){
+    window.open("https://test-zeta-swart-24.vercel.app/", '_blank');
+}
+function ouvrir_un_lien_github(){
+    window.open("https://github.com/", "_blank");
+}
